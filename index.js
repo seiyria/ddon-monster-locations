ead5940c7aaad4bffb866900b10abb253b366b84
Kyle Kemp <kyle@seiyria.com>
2017-11-18 15:14:10 -0600

    add hidell plains

---
+++ index.js
@@ -0,0 +1,83 @@
+
+Vue.component('multiselect', VueMultiselect.default);
+
+var vue = new Vue({
+    el: '#app',
+    data: {
+        search: 'goblin',
+        currentMonsters: [],
+        allMonsters: [],
+        noExtraData: true
+    },
+    methods: {
+        updateHash() {
+            updateWindowHash();
+        }
+    }
+});
+
+function formatData(data) {
+    return _(data.regions)
+        .map(({ name, subregions }) => {
+            var regionName = name;
+
+            return _.map(subregions, ({ name, location, level, monsters }) => {
+
+                var regionInfo = { name, location, level };
+
+                var sumName = name === regionName ? name : regionName + ' - ' + name;
+
+                return {
+                    regionName: sumName  + ' ~ ' + 'Lv. ' + level,
+                    monsterInfo: _.sortBy(_.map(monsters, ({ name, level, timePeriod, events, coordinates }) => {
+                        return {
+                            name: name + ' ~ Lv. ' + level,
+                            data: {
+                                name, level, timePeriod, events, coordinates
+                            },
+                            regionData: regionInfo
+                        };
+                    }), 'data.level')
+                };
+
+            });
+        })
+        .flattenDeep()
+        .value();
+}
+
+function loadPreviousMonsters() {
+    var loadMonster = window.location.hash;
+    if(!loadMonster) return;
+
+    var searchNames = loadMonster.substring(1).split('|');
+
+    if(!searchNames || !searchNames.length) return;
+
+    searchNames.forEach(searchName => {
+        var monsterObj = _(vue.allMonsters)
+            .map('monsterInfo')
+            .flattenDeep()
+            .filter({ name: searchName })
+            .value()[0];
+
+        if(!monsterObj) {
+            window.location.hash = '';
+            return;
+        }
+
+        vue.currentMonsters.push(monsterObj);
+    });
+
+}
+
+function updateWindowHash() {
+    var allMonsters = vue.currentMonsters.map(x => x.name).join('|');
+    window.location.hash = '#' + allMonsters;
+}
+
+axios.get('monsters.json')
+    .then(res => {
+        vue.allMonsters = formatData(res.data);
+        loadPreviousMonsters();
+    });
